a96dedbe5c4006dd9fff20ca08b95689bfd24501
naturalgroove <naturalgroove@gmail.com>
2024-10-28 18:05:36 +0100

    Added PostFactory and updated Post model and DatabaseSeeder

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\EloquentSortable\Sortable;
 use Spatie\EloquentSortable\SortableTrait;
 
 class Post extends Model implements Sortable
 {
+    use HasFactory;
     use SortableTrait;
 
     protected $guarded = [];
